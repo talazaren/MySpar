2033392464881c269b0fce4659196b386aac4e99
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-08-10 00:32:46 +0300

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Tatiana Lazarenko on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
